041013dbce704ab1e26ab45e95680be07330485a
Traci Ruffner <traci.ruffner@FASTLANEUS.COM>
2017-07-05 18:44:33 -0400

    adding sea creatures and changed title

--- js/index.js
+++ js/index.js
@@ -1,10 +1,30 @@
+
+
 $(document).ready(function(){
     
   $(function() {    
     $('#pod').draggable();
   });   
     
-  $('#bubble').click(function(){
-      $('#bubble').effect('explode');
-  });    
+  $('#bubble1').click(function(){
+      $('#bubble1').effect('explode');
+  });  
+    
+  $('#bubble2').click(function(){
+      $('#bubble2').effect('explode');
+  }); 
+    
+  $('#bubble3').click(function(){
+      $('#bubble3').effect('explode');
+  });   
+    
+  $('#bubble4').click(function(){
+      $('#bubble4').effect('explode');
+  });        
+    
+  $('#whale').click(function(){
+      $('#whale').effect('bounce', {times:10}, 2000);      
+  }); 
+    
+  
 });  
